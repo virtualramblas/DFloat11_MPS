1f80e5c1d40ba0c1c5e4642fa30ddaa4a2f11cbd
virtualramblas <guglielmo.iozzia@gmail.com>
2025-12-15 12:31:04 +0000

    Parameterized the seed.

--- inference.py
+++ inference.py
@@ -1,8 +1,8 @@
 import argparse
 from dfloat11mps.dfloat11 import DFloat11Model
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, set_seed
 
-def main(model_id, num_tokens, temperature, prompt):
+def main(model_id, num_tokens, temperature, prompt, seed):
     # Load model - will automatically use Swift Metal decoder and select MPS
     model = DFloat11Model.from_pretrained(model_id)
 
@@ -11,8 +11,14 @@ def main(model_id, num_tokens, temperature, prompt):
     inputs = tokenizer(prompt, return_tensors="pt")
     inputs.to('mps')
     
+    # Set random seed for deterministic sampling
+    set_seed(seed)
+
     # Model weights are decoded on-the-fly using Metal
-    outputs = model.generate(**inputs, max_length=num_tokens, temperature=temperature)
+    outputs = model.generate(**inputs, 
+                             max_length=num_tokens, 
+                             temperature=temperature,
+                             do_sample=True)
     print(tokenizer.decode(outputs[0]))
 
 if __name__ == '__main__':
@@ -21,7 +27,9 @@ if __name__ == '__main__':
     parser.add_argument('--prompt', type=str, default='Question: What is a binary tree and its applications? Answer:')
     parser.add_argument('--num_tokens', type=int, default=512)
     parser.add_argument('--temperature', type=int, default=0.2)
+    parser.add_argument('--seed', type=int, default=42)
     
     args = parser.parse_args()
 
-    main(args.model_id, args.num_tokens, args.temperature, args.prompt)
+    main(args.model_id, args.num_tokens, 
+         args.temperature, args.prompt, args.seed)
